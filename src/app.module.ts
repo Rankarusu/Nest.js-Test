b8cf6bed0d520683f42b28089be8cb60d830bb8e
Rankarusu <darkigfx@gmail.com>
2022-09-13 21:15:17 +0200

    cleaning up trash

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { PostService } from './post.service';
 import { PrismaService } from './prisma/prisma.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { AuthService } from './auth/auth.service';
 import { PostsModule } from './posts/posts.module';
 import { UsersService } from './users/users.service';
+import { PostsService } from './posts/posts.service';
 
 @Module({
   imports: [AuthModule, UsersModule, UsersModule, PostsModule],
   controllers: [AppController],
-  providers: [PrismaService, UsersService, PostService, AuthService],
+  providers: [PrismaService, UsersService, PostsService, AuthService],
 })
 export class AppModule {}
